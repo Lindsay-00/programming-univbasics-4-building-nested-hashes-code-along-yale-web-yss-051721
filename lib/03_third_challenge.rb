6c87de0903d70c494abaa96a6fcf40e3db074319
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-02 22:55:18 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,10 +18,11 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => 
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => 
         },
       :heroine => {
         #your key/value pairs here
